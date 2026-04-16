9c80cbb0ed68d0aa3fd50e786cc4b3f0890a4fce
abreuadrian <abreuadrianoliv@gmail.com>
2026-04-16 19:50:16 -0300

    add exps in exemplo07_pt2.py

---
+++ aula07/exemplo07_pt2.py
@@ -0,0 +1,34 @@
+#Ex01.: Altera item da lista
+def ex01():
+    list_prod = ['Notebook', 'Mouse', 'Teclado', 'Monitor']
+    print(list_prod)
+    list_prod[0] = 'PC'
+    print(f'Alterar o primeiro elemento {list_prod}')
+    print(list_prod)
+
+#Ex02.: Adiciona item na lista
+def ex02():
+    list_prod = ['Notebook', 'Mouse', 'Teclado', 'Monitor']
+    list_prod.append('WebCam')
+    print(list_prod)
+
+#Ex03.: Remove produto específico 
+def ex03():
+    list_prod = ['Notebook', 'Mouse', 'Teclado', 'Monitor']
+    print(list_prod)
+    list_prod.remove('Teclado')
+    print(list_prod)
+
+#Ex04.: Remove o último item da lista
+def ex04():
+    list_prod = ['Notebook', 'Mouse', 'Teclado', 'Monitor']
+    print(list_prod)
+    list_prod.pop()
+    print(list_prod)
+
+#Ex05.: Remove pelo índice
+def ex05():
+    list_prod = ['Notebook', 'Mouse', 'Teclado', 'Monitor']
+    print(list_prod)
+    del list_prod[1]
+    print(list_prod)
